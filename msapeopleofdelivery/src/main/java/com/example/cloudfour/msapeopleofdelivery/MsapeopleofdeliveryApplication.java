addfa69be92fbe3cedfa60b8e6919614cd11c679
shiien14 <98999139+shiien14@users.noreply.github.com>
2025-08-07 14:11:20 +0900

    🚀 스프링 프로젝트 생성

---
+++ msapeopleofdelivery/src/main/java/com/example/cloudfour/msapeopleofdelivery/MsapeopleofdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudfour.msapeopleofdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsapeopleofdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsapeopleofdeliveryApplication.class, args);
+	}
+
+}
