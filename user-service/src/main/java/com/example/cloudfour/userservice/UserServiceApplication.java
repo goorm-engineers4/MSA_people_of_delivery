95596af5167444ca219a1da7e6388672b1e63716
shieun <shieunmo@gmail.com>
2025-08-09 23:20:44 +0900

    ✨ feat : user 엔티티 MSA 모둘화 - 모듈 기본 설정(docker, application.yml) - auth, user 서비스 user-service 모듈로 분리

---
+++ user-service/src/main/java/com/example/cloudfour/userservice/UserServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.cloudfour.userservice;
+
+import com.example.modulecommon.ModuleCommonApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
---
+++ user-service/src/main/java/com/example/cloudfour/userservice/domain/user/enums/LoginType.java
@@ -0,0 +1,6 @@
+package com.example.cloudfour.userservice.domain.user.enums;
+
+public enum LoginType {
+    LOCAL,
+    GOOGLE
+}
