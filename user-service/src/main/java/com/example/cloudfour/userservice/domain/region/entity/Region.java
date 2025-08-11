726a74ac6f1c9f67560df7986a2ff44a84e1ac17
shieun <shieunmo@gmail.com>
2025-08-11 15:42:54 +0900

    🔨 refactor : Region 파싱 및 저장 흐름 리팩터링(엔티티) - entity si,gu,dong 유니크 제약 추가 - entity builder 삭제

--- user-service/src/main/java/com/example/cloudfour/userservice/domain/region/entity/Region.java
+++ user-service/src/main/java/com/example/cloudfour/userservice/domain/region/entity/Region.java
@@ -1,5 +1,7 @@
 package com.example.cloudfour.userservice.domain.region.entity;
 
+import com.example.cloudfour.userservice.domain.region.exception.RegionErrorCode;
+import com.example.cloudfour.userservice.domain.region.exception.RegionException;
 import com.example.cloudfour.userservice.domain.user.entity.UserAddress;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -9,6 +11,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,9 +25,13 @@ import java.util.UUID;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder
-@Table(name = "p_region")
+@Table(
+        name = "p_region",
+        uniqueConstraints = @UniqueConstraint(
+                name = "uk_region_sgd",
+                columnNames = {"si","gu","dong"}
+        )
+)
 public class Region {
     @Id
     @GeneratedValue
@@ -39,14 +46,15 @@ public class Region {
     @Column(nullable = false)
     private String dong;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "region", cascade = CascadeType.ALL)
-    @Builder.Default
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "region")
     private List<UserAddress> addresses = new ArrayList<>();
 
-    public static class RegionBuilder{
-        private RegionBuilder id(UUID id){
-            throw new UnsupportedOperationException("id 수정 불가");
-        }
+    public static Region ofRaw(String si, String gu, String dong) {
+        Region r = new Region();
+        r.si = si;
+        r.gu = gu;
+        r.dong = dong;
+        return r;
     }
 
 }
