cbb60786c9c63ccb301163ef9847b480a76d63bb
JUNE <jh010303@naver.com>
2025-08-11 13:30:07 +0900

    :sparkles: feat: cart-service baseEntity 사용

--- cart-service/src/main/java/com/example/cloudfour/cartservice/CartServiceApplication.java
+++ cart-service/src/main/java/com/example/cloudfour/cartservice/CartServiceApplication.java
@@ -3,9 +3,11 @@ package com.example.cloudfour.cartservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableJpaAuditing
 public class CartServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CartServiceApplication.class, args);
