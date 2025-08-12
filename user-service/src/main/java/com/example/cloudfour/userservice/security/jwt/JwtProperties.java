58149e0c196fd8db14490ce7669c32cb65b51578
shieun <shieunmo@gmail.com>
2025-08-12 14:01:19 +0900

    🔨 refactor : JWT 속성 추가 - JwtProperties, JwtUtil 파일 추가 - application.yml 내 JWT 속성 추가

---
+++ user-service/src/main/java/com/example/cloudfour/userservice/security/jwt/JwtProperties.java
@@ -0,0 +1,20 @@
+package com.example.cloudfour.userservice.security.jwt;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "jwt")
+@Getter
+public class JwtProperties {
+    private String secret;
+    private Long expiration;
+    private String refreshSecret;
+    private Long refreshExpiration;
+
+    public void setSecret(String secret){ this.secret = secret;}
+    public void setExpiration(Long expirateion){ this.expiration = expirateion; }
+    public void setRefreshSecret(String refreshSecret){ this.refreshSecret = refreshSecret; }
+    public void setRefreshExpiration(Long refreshExpiration){ this.refreshExpiration = refreshExpiration; }
+}
