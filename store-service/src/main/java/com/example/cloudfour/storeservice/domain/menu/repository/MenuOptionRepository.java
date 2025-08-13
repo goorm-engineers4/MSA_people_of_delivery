09fe3d6c43ff643465b55c3e0a0de62c7841a193
JUNE <jh010303@naver.com>
2025-08-13 17:40:44 +0900

    :sparkles: feat: MenuOption에 queryDSL 추가

--- store-service/src/main/java/com/example/cloudfour/storeservice/domain/menu/repository/MenuOptionRepository.java
+++ store-service/src/main/java/com/example/cloudfour/storeservice/domain/menu/repository/MenuOptionRepository.java
@@ -1,19 +1,11 @@
 package com.example.cloudfour.storeservice.domain.menu.repository;
 
 import com.example.cloudfour.storeservice.domain.menu.entity.MenuOption;
+import com.example.cloudfour.storeservice.domain.menu.repository.querydsl.MenuOptionQueryDslRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.UUID;
 
-public interface MenuOptionRepository extends JpaRepository<MenuOption, UUID> {
+public interface MenuOptionRepository extends JpaRepository<MenuOption, UUID> , MenuOptionQueryDslRepository {
 
-    List<MenuOption> findByMenuIdOrderByAdditionalPrice(UUID menuId);
-
-    boolean existsByMenuIdAndOptionName(UUID menuId, String optionName);
-
-    @Query("SELECT mo FROM MenuOption mo JOIN FETCH mo.menu WHERE mo.id = :optionId")
-    java.util.Optional<MenuOption> findByIdWithMenu(@Param("optionId") UUID optionId);
 }
