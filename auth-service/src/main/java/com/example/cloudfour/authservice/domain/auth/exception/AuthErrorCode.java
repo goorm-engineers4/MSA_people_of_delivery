633da5394154ec91bf5b74026d1d4356875bcdfa
shieun <shieunmo@gmail.com>
2025-08-15 14:52:03 +0900

    🔨 refactor: autherrorcode, authexception 이동 및 errorcode 추가

--- auth-service/src/main/java/com/example/cloudfour/authservice/domain/auth/exception/AuthErrorCode.java
+++ auth-service/src/main/java/com/example/cloudfour/authservice/domain/auth/exception/AuthErrorCode.java
@@ -1,4 +1,4 @@
-package com.example.cloudfour.userservice.domain.auth.exception;
+package com.example.cloudfour.authservice.domain.auth.exception;
 
 import com.example.cloudfour.modulecommon.apiPayLoad.code.BaseErrorCode;
 import lombok.AllArgsConstructor;
@@ -21,7 +21,10 @@ public enum AuthErrorCode implements BaseErrorCode {
     EMAIL_SAME_AS_OLD(HttpStatus.BAD_REQUEST, "AUTH400_7", "기존 이메일과 동일합니다."),
     EMAIL_CHANGE_PENDING_MISMATCH(HttpStatus.BAD_REQUEST, "AUTH400_8", "변경 대기 중인 이메일이 일치하지 않습니다."),
     EMAIL_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "AUTH500_1", "이메일 발송 실패"),
-    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "AUTH400_9", "현재 비밀번호가 일치하지 않습니다.");
+    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "AUTH400_9", "현재 비밀번호가 일치하지 않습니다."),
+    TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "AUTH_012", "유효하지 않은 토큰입니다."),
+    REFRESH_NOT_MATCHED(HttpStatus.UNAUTHORIZED, "AUTH_013", "리프레시 토큰이 일치하지 않습니다."),
+    TOKEN_TYPE_INVALID(HttpStatus.UNAUTHORIZED, "AUTH_014", "토큰 타입이 올바르지 않습니다.");
 
     private final HttpStatus status;
     private final String code;
--- auth-service/src/main/java/com/example/cloudfour/authservice/domain/auth/exception/AuthException.java
+++ auth-service/src/main/java/com/example/cloudfour/authservice/domain/auth/exception/AuthException.java
@@ -1,4 +1,4 @@
-package com.example.cloudfour.userservice.domain.auth.exception;
+package com.example.cloudfour.authservice.domain.auth.exception;
 
 import com.example.cloudfour.modulecommon.apiPayLoad.code.BaseErrorCode;
 import com.example.cloudfour.modulecommon.apiPayLoad.exception.CustomException;
