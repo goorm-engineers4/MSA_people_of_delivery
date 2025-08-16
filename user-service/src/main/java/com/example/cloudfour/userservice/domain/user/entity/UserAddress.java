f0ca88a450c504417ec1edf727561f6c6af5686b
shieun <shieunmo@gmail.com>
2025-08-16 23:40:44 +0900

    🔨 refactor: 충돌 해결

--- user-service/src/main/java/com/example/cloudfour/userservice/domain/user/entity/UserAddress.java
+++ user-service/src/main/java/com/example/cloudfour/userservice/domain/user/entity/UserAddress.java
@@ -3,7 +3,16 @@ package com.example.cloudfour.userservice.domain.user.entity;
 
 import com.example.cloudfour.userservice.domain.region.entity.Region;
 import com.example.cloudfour.userservice.domain.user.enums.AddressStatus;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
