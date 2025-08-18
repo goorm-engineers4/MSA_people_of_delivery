19f85e04df7f5ae3182af3811b1d7ad51fb23210
JUNE <jh010303@naver.com>
2025-08-18 10:27:35 +0900

    :sparkles: feat: ReviewDocument 조회 전용 repository 추가
    - QueryDSL사용

---
+++ store-service/src/main/java/com/example/cloudfour/storeservice/domain/collection/repository/ReviewMongoQueryRepositoryImpl.java
@@ -0,0 +1,60 @@
+package com.example.cloudfour.storeservice.domain.collection.repository;
+
+import com.example.cloudfour.storeservice.domain.collection.document.ReviewDocument;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.example.cloudfour.storeservice.domain.collection.document.QReviewDocument.reviewDocument;
+
+
+@Repository
+@RequiredArgsConstructor
+public class ReviewMongoQueryRepositoryImpl implements ReviewMongoQueryRepository {
+
+    private final JPAQueryFactory query;
+
+    @Override
+    public Slice<ReviewDocument> findAllByUserId(UUID userId, LocalDateTime cursor, Pageable pageable) {
+        int pageSize = pageable.getPageSize();
+        List<ReviewDocument> stores = query.selectFrom(reviewDocument)
+                .where(reviewDocument.userId.eq(userId)
+                        , reviewDocument.createdAt.lt(cursor)).orderBy(reviewDocument.createdAt.desc()).limit(pageSize+1)
+                .fetch();
+
+        boolean hasNext = stores.size() > pageSize;
+        if(hasNext){
+            stores.remove(pageSize);
+        }
+        return new SliceImpl<>(stores,pageable,hasNext);
+    }
+
+    @Override
+    public Slice<ReviewDocument> findAllByStoreId(UUID storeId, LocalDateTime cursor, Pageable pageable) {
+        int pageSize = pageable.getPageSize();
+        List<ReviewDocument> stores = query.selectFrom(reviewDocument)
+                .where(reviewDocument.storeId.eq(storeId)
+                        , reviewDocument.createdAt.lt(cursor)).orderBy(reviewDocument.createdAt.desc()).limit(pageSize+1)
+                .fetch();
+
+        boolean hasNext = stores.size() > pageSize;
+        if(hasNext){
+            stores.remove(pageSize);
+        }
+        return new SliceImpl<>(stores,pageable,hasNext);
+    }
+
+    @Override
+    public Optional<ReviewDocument> findById(UUID reviewId) {
+        return Optional.ofNullable(query.selectFrom(reviewDocument).
+                where(reviewDocument.reviewId.eq(reviewId)).fetchFirst());
+    }
+}
