0fee22d0ed3f24c6fb2401319160c8e07541f104
shieun <shieunmo@gmail.com>
2025-08-20 11:16:24 +0900

    ✨ feat: cart-service resttemplate 수정

--- cart-service/src/main/java/com/example/cloudfour/cartservice/config/RestTemplateConfig.java
+++ cart-service/src/main/java/com/example/cloudfour/cartservice/config/RestTemplateConfig.java
@@ -1,14 +1,21 @@
 package com.example.cloudfour.cartservice.config;
 
+import com.example.cloudfour.modulecommon.error.RestTemplateResponseErrorHandler;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    @LoadBalanced
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.errorHandler(new RestTemplateResponseErrorHandler()).build();
     }
 }
+
