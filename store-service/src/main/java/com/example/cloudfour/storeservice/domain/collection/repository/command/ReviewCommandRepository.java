b27f4a02df2702bd8035f936a5243edc552de02a
JUNE <jh010303@naver.com>
2025-08-22 13:53:13 +0900

    :sparkles: feat: MongoDB 동기화 Repository

--- store-service/src/main/java/com/example/cloudfour/storeservice/domain/collection/repository/command/ReviewCommandRepository.java
+++ store-service/src/main/java/com/example/cloudfour/storeservice/domain/collection/repository/command/ReviewCommandRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.UUID;
 
-public interface ReviewCommandRepository extends MongoRepository<ReviewDocument, UUID> {
+public interface ReviewCommandRepository extends MongoRepository<ReviewDocument, UUID>,ReviewCustomCommandRepository {
+
 }
--- store-service/src/main/java/com/example/cloudfour/storeservice/domain/collection/repository/command/StoreCommandRepository.java
+++ store-service/src/main/java/com/example/cloudfour/storeservice/domain/collection/repository/command/StoreCommandRepository.java
@@ -3,7 +3,9 @@ package com.example.cloudfour.storeservice.domain.collection.repository.command;
 import com.example.cloudfour.storeservice.domain.collection.document.StoreDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface StoreCommandRepository extends MongoRepository<StoreDocument, UUID> {
+    void deleteAllByStoreIdIn(List<UUID> ids);
 }
