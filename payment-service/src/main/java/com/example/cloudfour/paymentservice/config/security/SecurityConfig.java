bce221a0bfad1838ee7ff843f3b83403ad6526ee
shieun <shieunmo@gmail.com>
2025-08-25 13:49:55 +0900

    ✨ feat: securityconfig 설정

--- payment-service/src/main/java/com/example/cloudfour/paymentservice/config/security/SecurityConfig.java
+++ payment-service/src/main/java/com/example/cloudfour/paymentservice/config/security/SecurityConfig.java
@@ -25,7 +25,8 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/internal/**", "/actuator/**", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
-                        .requestMatchers("/carts/**","/cartItems/**","/orders/**","/profile/**").authenticated()
+                        .requestMatchers("/api/payments/webhook").permitAll()
+                        .requestMatchers("/api/payments/**").authenticated()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtClaimsAuthFilter, UsernamePasswordAuthenticationFilter.class)
