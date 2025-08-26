4b085a6ce01d20451266c74a8b355354bd16263d
JUNE <jh010303@naver.com>
2025-08-26 09:53:28 +0900

    :sparkles: feat: 서비스 내부에서 사용되는 stockController
    - cart-service에서 재고 감소,증가,조회 사용

--- store-service/src/main/java/com/example/cloudfour/storeservice/domain/menu/controller/InternalStockController.java
+++ store-service/src/main/java/com/example/cloudfour/storeservice/domain/menu/controller/InternalStockController.java
@@ -1,34 +1,44 @@
 package com.example.cloudfour.storeservice.domain.menu.controller;
 
 import com.example.cloudfour.storeservice.domain.menu.dto.StockResponseDTO;
-import com.example.cloudfour.storeservice.domain.menu.entity.Menu;
-import com.example.cloudfour.storeservice.domain.menu.entity.Stock;
-import com.example.cloudfour.storeservice.domain.menu.exception.MenuErrorCode;
-import com.example.cloudfour.storeservice.domain.menu.exception.MenuException;
-import com.example.cloudfour.storeservice.domain.menu.exception.StockErrorCode;
-import com.example.cloudfour.storeservice.domain.menu.exception.StockException;
-import com.example.cloudfour.storeservice.domain.menu.repository.MenuRepository;
-import com.example.cloudfour.storeservice.domain.menu.repository.StockRepository;
+import com.example.cloudfour.storeservice.domain.menu.service.command.StockCommandService;
+import com.example.cloudfour.storeservice.domain.menu.service.query.StockQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/internal/stocks")
+@RequestMapping("/internal")
 public class InternalStockController {
-    private final StockRepository query;
-    private final MenuRepository menuRepository;
 
-    @GetMapping("/{menuId}")
-    public StockResponseDTO getMenuDetail ( @PathVariable("menuId") UUID menuId ) {
-        Menu menu = menuRepository.findById(menuId).orElseThrow(()->new MenuException(MenuErrorCode.NOT_FOUND));
-        UUID stockId = menu.getStock().getId();
-        Stock stock = query.findByIdWithOptimisticLock(stockId).orElseThrow(()->new StockException(StockErrorCode.NOT_FOUND));
-        return StockResponseDTO.builder().quantity(stock.getQuantity()).build();
+    private final StockQueryService stockQueryService;
+    private final StockCommandService stockCommandService;
+
+    @GetMapping("/menus/{menuId}/stock")
+    public StockResponseDTO getMenuStock ( @PathVariable("menuId") UUID menuId ) {
+        return stockQueryService.getMenuStock(menuId);
+    }
+
+    @PostMapping("/menus/stock/{stockId}/decrease")
+    public void decreaseStock(
+            @PathVariable("stockId") UUID stockId,
+            @RequestParam(name = "quantity") Long quantity
+    ){
+        stockCommandService.decreaseStock(stockId,quantity);
+    }
+
+    @PostMapping("/menus/stock/{stockId}/increase")
+    public void increaseStock(
+            @PathVariable("stockId") UUID stockId,
+            @RequestParam(name = "quantity") Long quantity
+    ){
+        stockCommandService.increaseStock(stockId,quantity);
     }
 }
